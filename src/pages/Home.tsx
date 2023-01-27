3fa327ba5159de269945772a022b5208fb46fce0
MJ Sung <themrsung@gmail.com>
2023-01-28 06:33:12 +0900

    axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,27 @@
-import { useEffect } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { Auth } from "../classes/Auth"
+import { User } from "../classes/User"
 
 export default function Home() {
     const navigate = useNavigate()
 
+    const [users, setUsers] = useState<User[]>([])
+    const fetchUsers = async () => {
+        const res = await axios.get("http://localhost:5000/users")
+        setUsers(res.data as User[])
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
     useEffect(() => {
         Auth.onRestrictedPageLoad(navigate)
-    })
+    }, [])
+
+    console.log(users)
 
     return <>Home</>
 }
