2a3414fe0c3cf10dc3927d8c29812aa06cc3346a
MJ Sung <themrsung@gmail.com>
2023-01-28 06:33:06 +0900

    axios

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,8 +1,6 @@
-import { useEffect } from "react"
 import { Route, Routes } from "react-router"
 import { BrowserRouter } from "react-router-dom"
 import styled from "styled-components"
-import { Auth } from "../classes/Auth"
 import Home from "../pages/Home"
 import Landing from "../pages/Landing"
 import Login from "../pages/Login"
