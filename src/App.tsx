a1af291cb98b12f42d00a4c3340ade44bf9c7855
MJ Sung <themrsung@gmail.com>
2023-01-30 09:28:05 +0900

    App to class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import React, { useState } from "react"
 import "./App.css"
+import { State } from "./classes/State"
+import { User } from "./classes/User"
 import Router from "./components/Router"
 
-function App() {
-    return (
-        <>
-            <Router />
-        </>
-    )
+// function App() {
+//     const [state, setState] = useState(new State([]))
+
+//     console.log(state)
+//     console.log("users", state.getUsers())
+//     state.setUsers([new User("test", "test")])
+//     console.log(state)
+
+//     return (
+//         <>
+//             <Router />
+//         </>
+//     )
+// }
+
+class App extends React.Component {
+    state = new State([])
+
+    render() {
+        this.state.setCurrentUser(new User("name", "nme"))
+        console.log(this.state)
+        return <Router />
+    }
 }
 
 export default App
