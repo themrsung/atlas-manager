8c9bba00634ac9a4c858ab36005f8054b81cc540
MJ Sung <themrsung@gmail.com>
2023-02-23 23:24:34 +0900

    added welcome message

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,10 @@ export default function Home(props: { state: AtlasClientState }) {
             {state.getCurrentUser() ? (
                 <p>Welcome, {state.getCurrentUser()?.getDisplayName()}!</p>
             ) : (
-                <p>Welcome to Atlas Manager!</p>
+                <p>
+                    Welcome to Atlas Manager! Login to use or try it out by
+                    clicking [Console]. Warning: your data will be lost!
+                </p>
             )}
         </S.Wrap>
     )
