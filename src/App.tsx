52703e45b8005885b7a8dc87ea6fc1b9f4488a56
MJ Sung <themrsung@gmail.com>
2023-02-23 23:19:38 +0900

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from "react"
 import "./App.css"
 import AtlasClientState from "./classes/client/AtlasClientState"
-import AtlasClientUser from "./classes/client/AtlasClientUser"
-import AtlasClientDatabase from "./classes/client/AtlasClientDatabase"
-import AtlasClientEntry from "./classes/client/AtlasClientEntry"
-import AtlasClientEntryProperty from "./classes/client/AtlasClientEntryProperty"
 import Router from "./components/router/Router"
 import StyleConventions from "./style/StyleConventions"
-import Auth from "./api/Auth"
 
 class App extends React.Component {
     state: AtlasClientState = new AtlasClientState(this)
