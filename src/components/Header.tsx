ad12739f4e7373c5f229282e76cbbc1d08c6c7af
MJ Sung <themrsung@gmail.com>
2023-02-27 18:39:05 +0900

    .0.0.0.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,6 @@ import S from "../style/components/HeaderStyles"
 import StyleConventions from "../style/StyleConventions"
 import { RouteNames } from "./router/RouteNames"
 import darkLogoOnly from "../assets/logos/darkLogoOnly.svg"
-import MessageWindowProperties from "../classes/client/messageWindow/MessageWindowProperties"
-import MessageWindowButtonProperties from "../classes/client/messageWindow/MessageWindowButtonProperties"
-import { MessageWindowButtonType } from "../classes/client/messageWindow/MessageWindowButtonProperties"
 
 export default function Header(props: { state: AtlasClientState }) {
     const state = props.state
@@ -23,37 +20,6 @@ export default function Header(props: { state: AtlasClientState }) {
                 <S.Title>Atlas Manager</S.Title>
             </S.Left>
             <S.Right>
-                <StyleConventions.LargePositiveButton
-                    onClick={() => {
-                        state.setMessageWindowProperties(
-                            new MessageWindowProperties(
-                                state.getReactComponent(),
-                                true,
-                                "TESTTETSETSET",
-                                [
-                                    new MessageWindowButtonProperties(
-                                        state.getReactComponent(),
-                                        "button 1",
-                                        () => {
-                                            alert("1")
-                                        },
-                                        MessageWindowButtonType.Primary
-                                    ),
-                                    new MessageWindowButtonProperties(
-                                        state.getReactComponent(),
-                                        "button 2",
-                                        () => {
-                                            alert("2")
-                                        },
-                                        MessageWindowButtonType.Error
-                                    )
-                                ]
-                            )
-                        )
-                    }}
-                >
-                    TEST
-                </StyleConventions.LargePositiveButton>
                 <StyleConventions.MediumSecondaryButton
                     onClick={() => {
                         navigate(RouteNames.Home)
